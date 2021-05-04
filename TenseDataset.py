77ab39898055ca11c704d48fc5c32343edc0b1ab
adkevin3307 <adkevin3307@gmail.com>
2021-05-04 12:37:18 +0800

    add TenseDataset

---
+++ TenseDataset.py
@@ -0,0 +1,24 @@
+from typing import Callable
+from torch.utils.data import Dataset
+
+
+class TenseDataset(Dataset):
+    def __init__(self, path: str, transform: Callable = None) -> None:
+        self.path = path
+        self.transform = transform
+
+        self.data = []
+        with open(self.path, 'r') as txt_file:
+            for line in txt_file:
+                self.data.append(line.split())
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, index: int) -> tuple[str, int]:
+        row = index % len(self.data)
+        col = index // len(self.data)
+
+        word = self.data[row][col]
+
+        return (word, col)
