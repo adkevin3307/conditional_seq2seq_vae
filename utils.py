b198d0a6919d4d2245fdea48e08d5b537a1d8a94
adkevin3307 <adkevin3307@gmail.com>
2021-05-06 01:47:22 +0800

    add utils.py

---
+++ utils.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+def parse() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-e', '--epochs', type=int, default=100)
+    parser.add_argument('-m', '--max_length', type=int, default=15)
+    parser.add_argument('-s', '--size', type=int, dest='hidden_size', default=256)
+    parser.add_argument('-n', '--num_layers', type=int, default=2)
+    parser.add_argument('-a', '--annealing', type=str, default='cyclical', choices=['monotonic', 'cyclical'])
+    parser.add_argument('-p', '--path', type=str, default='weights')
+
+    args = parser.parse_args()
+
+    print('=' * 50)
+    for key, value in vars(args).items():
+        print(f'{key}: {value}')
+    print('=' * 50)
+
+    return args
