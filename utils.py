397f4f930c05282e52296dce2b6c062e959f31a3
adkevin3307 <adkevin3307@gmail.com>
2021-05-06 04:30:38 +0800

    update parser

--- utils.py
+++ utils.py
@@ -5,11 +5,11 @@ def parse() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-e', '--epochs', type=int, default=100)
-    parser.add_argument('-m', '--max_length', type=int, default=15)
     parser.add_argument('-s', '--size', type=int, dest='hidden_size', default=256)
     parser.add_argument('-n', '--num_layers', type=int, default=2)
     parser.add_argument('-a', '--annealing', type=str, default='cyclical', choices=['monotonic', 'cyclical'])
     parser.add_argument('-p', '--path', type=str, default='weights')
+    parser.add_argument('-t', '--trainable', action='store_true')
 
     args = parser.parse_args()
 
