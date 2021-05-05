74c9b53498a7413d718be3781016d2aed2e9942e
adkevin3307 <adkevin3307@gmail.com>
2021-05-06 02:25:40 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import random
 import numpy as np
-import argparse
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,27 +8,9 @@ from torch.utils.data import DataLoader
 from nltk.translate.bleu_score import SmoothingFunction, sentence_bleu
 
 import Constant
+from utils import parse
 from TenseDataset import Word2Index, Index2Word, TenseDataset
 from Net import TenseEncoder, TenseDecoder
-
-
-def parse() -> argparse.Namespace:
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-m', '--max_length', type=int, default=15)
-    parser.add_argument('-s', '--size', type=int, dest='hidden_size', default=256)
-    parser.add_argument('-n', '--num_layers', type=int, default=2)
-    parser.add_argument('-a', '--annealing', type=str, default='cyclical', choices=['monotonic', 'cyclical'])
-    parser.add_argument('-p', '--path', type=str, default='weights')
-
-    args = parser.parse_args()
-
-    print('=' * 50)
-    for key, value in vars(args).items():
-        print(f'{key}: {value}')
-    print('=' * 50)
-
-    return args
 
 
 def belu4(predict: torch.Tensor, truth: torch.Tensor) -> float:
@@ -37,13 +18,13 @@ def belu4(predict: torch.Tensor, truth: torch.Tensor) -> float:
     y_truth = Index2Word()(truth)
 
     score = 0.0
-    for y_hat, y in zip(y_predict, y_truth):
+    for s1, s2 in zip(y_predict, y_truth):
         cc = SmoothingFunction()
         weights = (0.33, 0.33, 0.33) if len(truth) == 3 else (0.25, 0.25, 0.25, 0.25)
 
-        score += sentence_bleu([y], y_hat, weights=weights, smoothing_function=cc.method1)
+        score += sentence_bleu([s2], s1, weights=weights, smoothing_function=cc.method1)
 
-    return score
+    return (score / len(y_truth))
 
 
 if __name__ == '__main__':
@@ -63,27 +44,32 @@ if __name__ == '__main__':
     train_loader = DataLoader(train_set, batch_size=32, num_workers=8, shuffle=True)
 
     encoder = TenseEncoder(input_size=Constant.VOCABULARY_SIZE, hidden_size=hidden_size, num_layers=num_layers)
-    decoder = TenseDecoder(output_size=Constant.VOCABULARY_SIZE, hidden_size=hidden_size, num_layers=num_layers, max_length=max_length)
+    decoder = TenseDecoder(output_size=Constant.VOCABULARY_SIZE, hidden_size=hidden_size, num_layers=num_layers)
 
     encoder, decoder = encoder.to(device), decoder.to(device)
 
-    encoder_optimizer = optim.SGD(encoder.parameters(), lr=Constant.LR)
-    decoder_optimizer = optim.SGD(encoder.parameters(), lr=Constant.LR)
+    encoder.train()
+    decoder.train()
+
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=Constant.LR, momentum=0.9)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=Constant.LR, momentum=0.9)
 
     criterion = nn.CrossEntropyLoss()
 
-    kld_alpha = 0.0
-    epoch_length = len(str(Constant.EPOCHS))
+    period = max(1, args.epochs // 10)
+    kld_alpha = 1.0 if period == 1 else 0.0
+    epoch_length = len(str(args.epochs))
 
-    for epoch in range(Constant.EPOCHS):
+    for epoch in range(args.epochs):
 
         kld_loss = 0.0
         ce_loss = 0.0
         belu4_score = 0.0
         accuracy = 0
+        tf_rate = 0.5
 
         monitor = {}
-        monitor_index = random.randint(0, len(train_loader))
+        monitor_index = random.randint(0, len(train_loader) - 1)
 
         for i, (word, tense) in enumerate(train_loader):
             word, tense = word.to(device), tense.to(device)
@@ -92,66 +78,75 @@ if __name__ == '__main__':
             decoder_optimizer.zero_grad()
 
             latent, (mu, logvar) = encoder(word, tense)
-            temp_kld_loss = kld_alpha * (-0.5 * torch.mean(1 + logvar - mu.pow(2) - torch.exp(logvar)))
+            temp_kld_loss = -0.5 * torch.mean(1 + logvar - mu.pow(2) - torch.exp(logvar))
+            kld_loss += temp_kld_loss.item()
 
-            enable_teacher_forcing = (random.random() < 0.5)
+            tf_rate = max(1.0 - (epoch + 1) / period, 0.5)
+            enable_tf = (random.random() < tf_rate)
 
-            input = torch.tensor([[Constant.SOS_TOKEN]]).repeat(1, latent.shape[1]).to(device)
+            input = torch.tensor([[Constant.SOS_TOKEN]]).repeat(1, word.shape[0]).to(device)
             hidden = (tense, latent)
             predict = []
 
+            temp_ce_loss = 0.0
             for j in range(word.shape[1]):
                 output, hidden = decoder(input, hidden)
                 predict.append(output)
 
-                if enable_teacher_forcing:
+                if enable_tf:
                     input = word[:, j].reshape(1, latent.shape[1])
                 else:
                     input = torch.argmax(output, dim=-1).type(torch.long)
 
+                temp_ce_loss += criterion(output.squeeze(), word[:, j])
+
+            ce_loss += temp_ce_loss
             predict = torch.cat(predict)
 
-            temp_ce_loss = criterion(predict.reshape(-1, predict.shape[-1]), torch.flatten(word))
-
-            temp_loss = temp_kld_loss + temp_ce_loss
-            kld_loss += temp_kld_loss.item()
-            ce_loss += temp_ce_loss.item()
-
             predict = torch.argmax(predict, dim=-1).transpose(0, 1)
-            accuracy += (predict == word).sum().item()
+            accuracy += sum(np.array(Index2Word()(word)) == np.array(Index2Word()(predict)))
 
-            if i == monitor_index:
-                monitor['word'] = np.squeeze(word.cpu().numpy())[0]
-                monitor['predict'] = np.squeeze(predict.cpu().numpy())[0]
-
-            temp_loss.backward()
+            (temp_kld_loss * kld_alpha + temp_ce_loss).backward()
             encoder_optimizer.step()
             decoder_optimizer.step()
+
+            if i == monitor_index:
+                monitor['word'] = word[0: 3]
+                monitor['pred'] = predict[0: 3]
 
             temp_belu4_score = belu4(predict, word)
             belu4_score += temp_belu4_score
 
             rate = (i + 1) / len(train_loader)
-            progress = f'Epochs: {(epoch + 1):>{epoch_length}} / {Constant.EPOCHS}, [{("=" * int(rate * 20)):<20}] {(rate * 100.0):>6.2f}%'
-            print(f'\r{progress}, kld_loss: {temp_kld_loss.item():.3f}, ce_loss: {temp_ce_loss.item():.3f}, belu4: {temp_belu4_score:.3f}', end='')
+            progress = f'Epochs: {(epoch + 1):>{epoch_length}} / {args.epochs}, [{("=" * int(rate * 20)):<20}] {(rate * 100.0):>6.2f}%'
+            status = f'tf_rate: {tf_rate:.3f}, kld_alpha: {kld_alpha:.3f}'
+
+            print(f'\r{progress}, {status}, kld_loss: {temp_kld_loss:.3f}, ce_loss: {temp_ce_loss:.3f}, belu4: {temp_belu4_score:.3f}', end='')
 
         kld_loss /= len(train_loader)
         ce_loss /= len(train_loader)
         belu4_score /= len(train_loader)
         accuracy /= len(train_set)
 
-        if args.annealing == 'monotonic':
-            kld_alpha = min(kld_alpha + (1.0 / 10000), 1.0)
-        elif args.annealing == 'cyclical':
-            kld_alpha = 0 if (((epoch + 1) % 10000) == 0) else min(kld_alpha + (1.0 / 5000), 1.0)
+        if (epoch + 1) % 1 == 0:
+            progress = f'Epochs: {(epoch + 1):>{epoch_length}} / {args.epochs}, [{("=" * 20)}]'
+            status = f'tf_rate: {tf_rate:.3f}, kld_alpha: {kld_alpha:.3f}'
 
-        if (epoch + 1) % 100 == 0:
-            progress = f'Epochs: {(epoch + 1):>{epoch_length}} / {Constant.EPOCHS}, [{("=" * 20)}]'
-            print(f'\r{progress}, kld_loss: {kld_loss:.3f}, ce_loss: {ce_loss:.3f}, belu4: {belu4_score:.3f}, accuracy: {accuracy:.3f}')
+            print(f'\r{progress}, {status}, kld_loss: {kld_loss:.3f}, ce_loss: {ce_loss:.3f}, belu4: {belu4_score:.3f}, accuracy: {accuracy:.3f}')
 
-        if (epoch + 1) % 1000 == 0:
-            print(f'   word: {monitor["word"]}')
-            print(f'predict: {monitor["predict"]}')
+        if (epoch + 1) % 10 == 0:
+            monitor['word'] = Index2Word()(monitor['word'])
+            monitor['pred'] = Index2Word()(monitor['pred'])
+
+            print(f'word: {", ".join(monitor["word"])}')
+            print(f'pred: {", ".join(monitor["pred"])}')
 
             torch.save(encoder, os.path.join(args.path, f'encoder_{epoch + 1}.weight'))
             torch.save(decoder, os.path.join(args.path, f'decoder_{epoch + 1}.weight'))
+
+        if period == 1:
+            kld_alpha = 1.0
+        elif args.annealing == 'monotonic':
+            kld_alpha = min(kld_alpha + (1.0 / period), 1.0)
+        elif args.annealing == 'cyclical':
+            kld_alpha = min(kld_alpha + (1.0 / (period / 2)), min((epoch + 1) % period, 1.0))
